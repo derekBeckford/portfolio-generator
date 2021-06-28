f1b65cf42463a7484f05c50032a6192dc86e5cd7
derekBeckford <82908627+derekBeckford@users.noreply.github.com>
2021-06-28 16:14:18 -0500

    beginning html

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require("fs");
 
-const printProfileData = (profileDataArr) => {
-  for (let i = 0; i < profileDataArr.length; i++) {
-    console.log(profileDataArr[i]);
-  }
-  console.log("================");
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const generatePage = require('./src/page-template.js');
 
-printProfileData(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+fs.writeFile("./index.html", generatePage(name, github), (err) => {
+  if (err) throw new Error(err);
+
+  console.log("Portfolio complete! Check out index.html to see the output!");
+});
